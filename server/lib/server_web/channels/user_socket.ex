1e16aa97cafa0a659e2165dfeeaed69f6fcd6944
Jordan West <jordwest@gmail.com>
2018-10-28 09:57:36 +1100

    enable websocket channel

--- server/lib/server_web/channels/user_socket.ex
+++ server/lib/server_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ServerWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ServerWeb.RoomChannel
+  channel "room:*", ServerWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
